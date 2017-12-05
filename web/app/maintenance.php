b29121b070d452c6207d066aaeb85043386a6415
le0daniel <leodaniel@nitrosoft.ch>
2017-12-05 16:30:30 +0100

    Fixed Maintenance mode

---
+++ web/app/maintenance.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kernbrand
+ * Date: 05.12.17
+ * Time: 15:20
+ */
+?>
+<html>
+
+<head>
+	<title>Maintenance</title>
+	<style>
+
+		body{
+			background-color: #efefef;
+			font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+			padding: 50px;
+		}
+		h1{
+			display: block;
+			text-align: center;
+			font-size: 55px;
+			line-height: 60px;
+			font-weight: 100;
+			color: #4c4c4c;
+		}
+
+	</style>
+</head>
+<body>
+
+	<h1>- We'll be back soon -</h1>
+
+</body>
+</html>
+<?php die(); ?>
