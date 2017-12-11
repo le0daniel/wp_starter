9469c8eacfc12e89170f65713e39267fc00ba887
le0daniel <leodaniel@nitrosoft.ch>
2017-12-11 15:56:53 +0100

    Removed unused gitkeep

--- web/app/themes/skeletton/App/config/app.php
+++ web/app/themes/skeletton/App/config/app.php
@@ -1,7 +1,13 @@
 <?php
 
 return [
+
+	/**
+	 * Add your service providers below,
+	 * They must implement the ServiceProvider Contract,
+	 * Should extend the abstract class RootServiceProvider
+	 */
 	'providers'=>[
 		\le0daniel\System\ServiceProviders\Log::class,
-	]
+	],
 ];
