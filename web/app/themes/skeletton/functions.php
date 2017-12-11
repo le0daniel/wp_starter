957780fd1562eb9d462ecc38676fdbc2d1c7791d
le0daniel <leodaniel@nitrosoft.ch>
2017-12-11 15:43:28 +0100

    Changed Documentation

--- web/app/themes/skeletton/functions.php
+++ web/app/themes/skeletton/functions.php
@@ -22,7 +22,7 @@ $view_root  = __DIR__.'/resources/views';
  * might need to init the App using App::init($root_dir)
  */
 if( ! class_exists(App::class) || ! function_exists('app') ){
-	echo 'Application not loaded!';
+	echo 'Application not loaded!'.PHP_EOL;
 	die();
 }
 
@@ -31,11 +31,28 @@ if( ! class_exists(App::class) || ! function_exists('app') ){
  * Paths & Namespaces
  * -------------------------------------------------------------
  * Register all needed Paths and namespaces
+ *
+ * You can use the Default config dir if you don't set the
+ * config dir! Not needed if you don't register any
+ * Service Provider
+ *
+ * Load the default Theme Text domain using the theme slug
+ * --> This should not be changed
+ *
+ * Sets the Theme dirname!
+ * Is used for path generateion! Do not change!
  */
 App::$config_dir = __DIR__.'/App/config';
 load_theme_textdomain($theme_name,__DIR__.'/resources/lang/');
 Language::$translation_context = $theme_name;
 Path::$theme_dirname = $theme_name;
+
+/**
+ * -------------------------------------------------------------
+ * Theme Support
+ * -------------------------------------------------------------
+ * Add theme support below
+ */
 //add_theme_support( 'title-tag' );
 //add_theme_support( 'html5', [ 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption', ]);
 
@@ -48,6 +65,10 @@ Path::$theme_dirname = $theme_name;
  * Here is the best point to change default classes like the
  * Wordpress\Context Class (extend|replace ...)
  * +--> bind('wp.context',MyClassWhichExtendsContext::class)
+ * +--> wp.page, wp.site, wp.user
+ *
+ * All classes are resolved through the ServiceContainer, so
+ * you can Type-Hint all dependencies!
  */
 $app = app();
 $app->bind(AddLogicToWordpress::class,WordPressExtender::class);
@@ -58,11 +79,11 @@ $app->bind(AddLogicToWordpress::class,WordPressExtender::class);
  * -------------------------------------------------------------
  * Configure the view object
  *
- * - Register needed Paths (especially @shorts for shortcuts)
+ * - Register needed Paths (especially @shorts for shortcuts_builder)
  * - Add Context (default wp.context), resolved through container, lazy loaded
  *   must Implement CastArray
  * - Share Data with all views
- * - Enable global plain cache ()
+ * - Enable global plain cache (Experimental!)
  */
 view()
 	->setRootDir($view_root)
@@ -90,5 +111,16 @@ view()
  * -------------------------------------------------------------
  * Runs the app, depending on mode, Http- or Cli Kernel is
  * loaded and runned!
+ *
+ * To follow the MVC Pattern, use the individual files as
+ * Controllers, which should only pass data to the view!
+ *
+ * You can use them as classes to, just declare a class and define
+ * a render method which should return an array [template, data]
+ * The controller Class and method is resolved through the container
+ * so you can Type Hint!
+ *
+ * To use this feature, set map_controllers_to_classes to true in
+ * the config file
  */
 $app->run();
